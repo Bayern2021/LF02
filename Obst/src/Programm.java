5e62c68688f237f2f4079c99c36de3ffc285c606
Mohamed <mohamed134@schule.bremen.de>
2022-01-13 15:19:57 +0100

    Programm.java

---
+++ Obst/src/Programm.java
@@ -0,0 +1,23 @@
+public class Programm {
+    public static void main(String[] args) {
+        Apfel apfel = new Apfel();
+        apfel.setFarbe("Grün");
+        apfel.setGewicht(250);
+        apfel.setReif(true);
+
+        System.out.println(apfel.getName());
+        System.out.println(apfel.getGewicht());
+
+        Banane banane = new Banane();
+        System.out.println(banane.getName());
+       
+
+
+
+
+
+
+
+
+    }
+}
